4ad1557fa6e9713d51b818d13dffa8a23dd2f77a
alxmra <miguelamneto@ua.pt>
2026-02-25 01:05:22 +0000

    adapt boilerplate to pykaf

--- client_sdk/policy_client/__init__.py
+++ client_sdk/policy_client/__init__.py
@@ -1,6 +1,19 @@
 """Policy Client SDK - for components to interact with the Policy Service."""
 
 from .client import PolicyClient, ProcessResult
+from .kafka_interceptor import (
+    PyKafBridgePolicyInterceptor,
+    create_pykafbridge_policy_consumer,
+    bind_policy_to_topic
+)
+from .middleware import PolicyMiddleware
 
-__all__ = ["PolicyClient", "ProcessResult"]
+__all__ = [
+    "PolicyClient",
+    "ProcessResult",
+    "PyKafBridgePolicyInterceptor",
+    "create_pykafbridge_policy_consumer",
+    "bind_policy_to_topic",
+    "PolicyMiddleware",
+]
 __version__ = "1.0.0"
